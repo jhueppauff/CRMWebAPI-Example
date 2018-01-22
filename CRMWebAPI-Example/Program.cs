5c022fc03b83dc707dbbc0fd9c628090a9811657
Julian Hüpppauff <julian@hueppauff.info>
2018-01-22 18:36:13 +0100

    Add project files.

---
+++ CRMWebAPI-Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRMWebAPI_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
